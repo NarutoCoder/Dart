e3eca092dd419492ecc70d340cddd7614fdb5ba5
NarutoCoder <81152878+NarutoCoder@users.noreply.github.com>
2021-03-22 17:00:34 +0530

    Update Learn.dart

--- Learn.dart
+++ Learn.dart
@@ -6,6 +6,16 @@ void main(){
 
   int sum = add(3 ,5);
   print('sum is $sum');
-
-
 }
+
+// some speciality of dart for functions 
+
+void main(){
+  manager(qnty:5,item:'sugar',units:'kg');
+}
+manager({int qnty, String units, String item}){
+
+  print('You ordered $qnty$units of $item');
+}
+
+  
